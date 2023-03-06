c102c7100573ee6ca87acd239dc7c7462212b734
Harshal Bondre <91895030+harshalbondre101@users.noreply.github.com>
2023-03-06 20:10:36 +0530

    Update add.php

--- add.php
+++ add.php
@@ -11,8 +11,6 @@ $science = $_POST['science'];
 $computer = $_POST['computer'];
 
 
-echo $roll;
-
 $sql = "INSERT INTO roll (roll,class,section,res,english,maths,language1,science,computer) VALUES ('$roll','$class','$section','$res','$english','$maths','$language1','$science','$computer')";
 
 if (mysqli_query($conn, $sql)) {
